6d289657ddca79423e5539d699a2be1b510a8b64
Gustavo Rodrigues <qgustavor-git-commits@blio.tk>
2015-11-27 10:25:52 -0200

    Improve anime colors
    Similar names have similar colors

--- animes/anime-color.js
+++ animes/anime-color.js
@@ -9,11 +9,13 @@ function adler32(data) {
   return a | (b << 16);
 }
 
-function getAnimeColor(name) {
-  var hash = adler32(name);
-  return tinycolor('#F00')
-    .desaturate(hash % 20)
-    .darken((hash / 20) % 20)
-    .spin((hash / 400) % 360)
+function getAnimeColor(name){
+  var normalized = name.toLowerCase();
+  return tinycolor('#F11')
+    .desaturate(
+      adler32(normalized.substr(5)) % 30
+    ).darken(
+      adler32(normalized.substr(1,4)) % 20
+    ).spin((normalized.charCodeAt(0) * 14.4) % 360)
     .toString();
 }
