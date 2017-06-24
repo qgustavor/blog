69fcbf94f77c19ca67d3d089bc8b5f50b6d467d3
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2017-06-24 10:34:10 -0300

    Add progress chart and change colors
    Colors are now picked from the spreadsheet, which come from
    the most vibrant colors in the anime posters.

--- animes/anime-color.js
+++ animes/anime-color.js
@@ -1,20 +1,46 @@
-var ADLER32_MOD = 65521
 function adler32 (data) {
   var a = 1
   var b = 0
   for (var i = 0; i < data.length; i++) {
-    a = (a + data.charCodeAt(i)) % ADLER32_MOD
-    b = (b + a) % ADLER32_MOD
+    a = (a + data.charCodeAt(i)) % 65521
+    b = (b + a) % 65521
   }
   return a | (b << 16)
 }
 
-function getAnimeColor (name) {
+function getAnimeColor (name, alternative, sheets) {
+  if (sheets) {
+    var keys = {
+      anime: 'Animes',
+      name: 'Nome',
+      color1: 'Cor 1',
+      color2: 'Cor 2'
+    }
+
+    var animeData = sheets[keys.anime].elements
+    for (var i = 0; i < animeData.length; i++) {
+      if (animeData[i][keys.name] === name) {
+        var color = alternative
+        ? animeData[i][keys.color2]
+        : animeData[i][keys.color1]
+
+        if (color) return color
+        break
+      }
+    }
+  }
+
   var normalized = name.toLowerCase()
-  return tinycolor('#F11')
+  var color = tinycolor('#F22')
     .desaturate(
       adler32(normalized.substr(5)) % 30
     ).darken(
       adler32(normalized.substr(1,4)) % 20
     ).spin((normalized.charCodeAt(0) * 14.4) % 360)
+
+  if (alternative) {
+    color = color.darken(8)
+  }
+
+  return color
 }
